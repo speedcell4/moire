f230c7a46b07d5299777f980035dfd0f8a42e958
speedcell4 <speedcell4@gmail.com>
2018-02-16 22:57:09 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='moire',
+    version='0.0.1',
+    packages=['moire'],
+    url='',
+    license='MIT',
+    author='speedcell4',
+    author_email='speedcell4@gmail.com',
+    description='DyNet interfaces for Humans',
+)
