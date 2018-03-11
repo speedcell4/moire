c7cd106098e6192f65801a9b8cb015e9624e364e
speedcell4 <speedcell4@gmail.com>
2018-03-11 15:02:01 +0900

    update noise.py

--- moire/nn/noise.py
+++ moire/nn/noise.py
@@ -1,5 +1,6 @@
 import dynet as dy
 
+import moire
 from moire import nn, where, bernoulli_like
 
 __all__ = [
@@ -8,31 +9,34 @@ __all__ = [
 
 
 class Dropout(nn.Function):
-    def __init__(self, ratio: float):
+    __slots__ = ('ratio',)
+
+    def __init__(self, ratio: float = None):
         super(Dropout, self).__init__()
         self.ratio = ratio
 
     def __call__(self, x):
-        if self.training and self.ratio > 0.0:
+        if self.ratio is not None and moire.config.train:
             return dy.dropout(x, self.ratio)
         return x
 
 
 class Zoneout(nn.Function):
-    def __init__(self, ratio: float):
+    __slots__ = ('ratio',)
+
+    def __init__(self, ratio: float = None):
         super(Zoneout, self).__init__()
-        self.ratio = 1.0 - ratio
+        self.ratio = None if ratio is None else 1.0 - ratio
 
     def __call__(self, x, h):
-        if self.training and self.ratio < 1.0:
+        if self.ratio is not None and moire.config.train:
             cond = bernoulli_like(x, self.ratio)
             return where(cond, x, h)
         return x
 
 
 if __name__ == '__main__':
-    zoneout = Zoneout(0.9)
-    zoneout.training = True
+    zoneout = Zoneout()
 
     x = dy.inputVector([1, 2, 3])
     h = dy.inputVector([7, 8, 9])
