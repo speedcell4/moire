fbad375f3fb2e0022c3db2cfcd70ca2d24e72bfb
speedcell4 <speedcell4@gmail.com>
2018-03-12 18:27:43 +0900

    success on training DQN and DoubleDQN

--- examples/train_dqn.py
+++ examples/train_dqn.py
@@ -8,46 +8,54 @@ app = aku.App(__file__)
 
 @app.register
 def train(device: str = 'CPU', gamma: float = 0.95, batch_size: int = 32, replay_start_size: int = 500,
-          target_update_interval: int = 100, start_epsilon: float = 0.3, epsilon_decay_interval: int = 50000,
-          epsilon_decay: float = 0.03, nb_iterations: int = 200000000, capacity: int = 10 ** 6, num_layers: int = 2,
-          hidden_size: int = 200, update_interval: int = 1):
+          target_update_interval: int = 100, start_epsilon: float = 0.3, n_episodes: int = 200,
+          max_episode_len: int = 200,
+          capacity: int = 10 ** 6, num_layers: int = 3,
+          hidden_size: int = 50, update_interval: int = 1):
     launch_moire.launch_moire(device)
 
-    import dynet as dy
     import moire
+    import dynet as dy
     from moire import ParameterCollection
     from moire import nn
+    from moire.nn.trigonometry import tanh
     from moire.nn.reinforces import DQN, ReplayBuffer
 
     moire.config.epsilon = start_epsilon
 
     pc = ParameterCollection()
-    q_function = nn.MLP(pc, num_layers, 4, 2, hidden_size)
+    q_function = nn.MLP(pc, num_layers, 4, 2, hidden_size, nonlinear=tanh)
 
-    optimizer = dy.AdamTrainer(pc)
+    optimizer = dy.AdamTrainer(pc, eps=1e-3)
 
-    target_q_function = nn.MLP(ParameterCollection(), num_layers, 4, 2, hidden_size)
+    target_q_function = nn.MLP(ParameterCollection(), num_layers, 4, 2, hidden_size, nonlinear=tanh)
     replay_buffer = ReplayBuffer(capacity)
 
     agent = DQN(q_function, target_q_function, replay_buffer, optimizer, gamma, batch_size,
-                replay_start_size, 1, update_interval, target_update_interval)
+                      replay_start_size, 1, update_interval, target_update_interval)
 
     env = gym.make('CartPole-v0')
-    for i_episode in range(1, nb_iterations):
-        obs, reward, done = env.reset(), 0.0, False
+    for i in range(1, n_episodes + 1):
+        obs = env.reset()
+        reward = 0
+        done = False
+        R = 0  # return (sum of rewards)
+        t = 0  # time step
 
-        while not done:
+        while not done and t < max_episode_len:
+            dy.renew_cg()
             action = agent.act_and_train(dy.inputVector(obs), reward)
             obs, reward, done, _ = env.step(action)
 
+            R += reward
+            t += 1
+        if i % 10 == 0:
+            print('episode:', i,
+                  'R:', R,
+                  f'average_q: {agent.average_q:.03f}',
+                  f'average_loss: {agent.average_loss:.03f}')
         agent.stop_episode_and_train(dy.inputVector(obs), reward, done)
-
-        if i_episode % epsilon_decay_interval == 0:
-            moire.config.epsilon -= epsilon_decay
-            print(f'epsilon => {moire.config.epsilon:.03f}', file=moire.config.stdlog)
-
-        print(f'{i_episode:010d} :: average_q => {agent.average_q:.03f}, average_l => {agent.average_loss:.03f}',
-              file=moire.config.stdlog)
+    print('Finished.')
 
 
 if __name__ == '__main__':
