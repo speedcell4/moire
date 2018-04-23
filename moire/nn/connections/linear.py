d5acaee4ee0690627f1382debdd6a6f11cec77c7
speedcell4 <speedcell4@gmail.com>
2018-04-23 15:45:21 +0900

    ✨ beautiful old days

--- moire/nn/connections/linear.py
+++ moire/nn/connections/linear.py
@@ -15,21 +15,23 @@ class Linear(nn.Module):
         self.use_bias = use_bias
 
         self.W = self.add_param((out_feature, in_feature), weight_initializer)
-        if not use_bias:
-            bias_initializer = Zero()
-        self.b = self.add_param((out_feature,), bias_initializer)
+        if use_bias:
+            self.b = self.add_param((out_feature,), bias_initializer)
 
     def __repr__(self):
         return f'{self.__class__.__name__} ({self.in_feature} -> {self.out_feature})'
 
     def __call__(self, x: Expression) -> Expression:
         W = self.W.expr(moire.config.train)
-        b = self.b.expr(self.use_bias and moire.config.train)
-        return dy.affine_transform([b, W, x])
+        if self.use_bias:
+            b = self.b.expr(moire.config.train)
+            return dy.affine_transform([b, W, x])
+        else:
+            return W * x
 
 
 if __name__ == '__main__':
-    fc = Linear(ParameterCollection(), 4, 3)
+    fc = Linear(ParameterCollection(), 4, 3, use_bias=False)
     dy.renew_cg()
 
     x = dy.inputVector([1, 2, 3, 4])
